c6d295ed5b7032bfe48c2aaf3e9026da0d5d3048
Nocciiii <47415855+Nocciiii@users.noreply.github.com>
2019-05-20 12:40:47 +0200

    Anpassen des createRow und playerHit

--- SpaceInvaders/View/Alien.cs
+++ SpaceInvaders/View/Alien.cs
@@ -1,16 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using View;
 
 namespace Model
 {
-    public class Alien
+    public class Alien : INotifyPropertyChanged
     {
         private Uri look = new Uri("Alien1.png", UriKind.Relative);
+        private static int currentId = 0;
+        private int id;
+        private int currentRow;
+        private Double xpos;
+        private Double ypos;
+        public event PropertyChangedEventHandler PropertyChanged;
 
+        public Alien(Double xpos, Double ypos, int currentRow)
+        {
+            this.Xpos = xpos;
+            this.Ypos = ypos;
+            this.CurrentRow = currentRow;
+            this.id = currentId;
+            currentId++;
+        }
+
+        protected void OnPropertyChanged(String name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
 
         public Uri Look { get => look; set => look = value; }
+        public double Xpos { get => xpos; set { xpos = value; OnPropertyChanged("Xpos"); } }
+        public double Ypos { get => ypos; set { ypos = value; OnPropertyChanged("Ypos"); } }
+        public int Id { get => id; }
+        public int CurrentRow { get => currentRow; set => currentRow = value; }
     }
 }
