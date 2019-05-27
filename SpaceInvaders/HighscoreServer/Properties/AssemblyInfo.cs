b68f00faaf5b5d100dc9855f1fee08ae68799a46
KuemmertJamie <47415865+KuemmertJamie@users.noreply.github.com>
2019-05-27 12:36:49 +0200

    Anfang der Serververbindung für das Auslesen der Highscores

---
+++ SpaceInvaders/HighscoreServer/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("HighscoreServer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HighscoreServer")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("fe7852c8-6cc2-4c61-9049-73f8ded9c5b4")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SpaceInvaders/HighscoreServer/Server.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighscoreServer
+{
+    class Server
+    {
+        private int port = 8008;
+        private String host = "localhost";
+        Socket socket = null;
+
+        static void Main(string[] args)
+        {
+            new Server();
+        }
+        public Server()
+        {
+            running();
+        }
+        public void running()
+        {
+            IPHostEntry hostEntry = Dns.GetHostEntry(host);
+            IPAddress[] ipAddresses = hostEntry.AddressList;
+
+            Console.WriteLine(host + " is mapped to the IP-Address(es): ");
+
+            // Ausgabe der zugeordneten IP-Adressen
+            foreach (IPAddress ipAddress in ipAddresses)
+            {
+                Console.Write(ipAddress.ToString());
+            }
+
+            // Instanziere einen Endpunkt mit der ersten IP-Adresse
+            IPEndPoint ipEo = new IPEndPoint(ipAddresses[0], port);
+
+            while (true)
+            {
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(ipEo);
+
+                socket.Close();
+            }
+        }
+    }
+}
