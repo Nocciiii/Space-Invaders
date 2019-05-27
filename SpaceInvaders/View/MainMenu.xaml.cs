2d04a4a97c17194b4dac22bcf81b7f36f61decef
KuemmertJamie <47415865+KuemmertJamie@users.noreply.github.com>
2019-05-27 11:59:35 +0200

    Hinzufügen eines Hauptmenüs(Startpunkt des Ausführens muss noch festgelegt werden)

---
+++ SpaceInvaders/View/MainMenu.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace View
+{
+    /// <summary>
+    /// Interaktionslogik für MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Window
+    {
+        
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow game = new MainWindow();
+            this.Visibility=Visibility.Hidden;
+            game.Visibility = Visibility.Visible;
+        }
+
+        private void HighscoreButton_Click(object sender, RoutedEventArgs e)
+        {
+            HighscoreList highscores = new HighscoreList();
+            this.Visibility = Visibility.Hidden;
+            highscores.Visibility = Visibility.Visible;
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
